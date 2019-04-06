fbff10b1112444d569079cf39cb3cbe2d83bb1e5
AlejandroOnofre <alejandro.onofre@bloque9.com>
2019-04-05 18:27:07 -0600

    ADD project heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 1, name: 'Mr. Nice' },
+	{ id: 2, name: 'Marc' },
+	{ id: 3, name: 'Dr.IQ' },
+	{ id: 4, name: 'Magma' },
+	{ id: 5, name: 'Tornado' },
+	{ id: 6, name: 'Storm' },
+	{ id: 7, name: 'Dynama' },
+	{ id: 8, name: 'RubberMan' },
+	{ id: 9, name: 'Magneto' },
+	{ id: 10, name: 'Mexico' },
+	{ id: 11, name: 'Super' },
+	{ id: 12, name: 'Bat' },
+	{ id: 13, name: 'Mr.' },
+	{ id: 14, name: 'Dr' },
+];
